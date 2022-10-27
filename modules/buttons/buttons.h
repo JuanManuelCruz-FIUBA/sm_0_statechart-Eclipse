4dac338bd6c1869780975dd38a651426feffe1ed
JuanManuelCruz-FIUBA <98561166+JuanManuelCruz-FIUBA@users.noreply.github.com>
2022-10-27 11:52:57 -0300

    Commit # 0

---
+++ modules/buttons/buttons.h
@@ -0,0 +1,18 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _BUTTONS_H_
+#define _BUTTONS_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void opButtonInit();
+void opButtonUpdate();
+int opButtonRead();
+
+//=====[#include guards - end]=================================================
+
+#endif // _BUTTONS_H_
---
+++ modules/leds/leds.h
@@ -0,0 +1,19 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _LEDS_H_
+#define _LEDS_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void opLedInit();
+void opLedUpdate();
+int opLedRead();
+void opLedWrite( int );
+
+//=====[#include guards - end]=================================================
+
+#endif // _LEDS_H_
